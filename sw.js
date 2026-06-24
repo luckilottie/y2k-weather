83829c0bd835daf0d78466412992ee94397d1210
Lucki lottie <wispybabie@gmail.com>
2026-06-23 23:16:03 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'mermaid-weather-v1';
+const urlsToCache = [
+  '/y2k-weather/',
+  '/y2k-weather/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
